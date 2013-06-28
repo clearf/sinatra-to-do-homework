1bc6f05f9b868f99c7ed928e5acb13e4c276ac64
Alina Guzman <alinochka8@gmail.com>
2013-06-28 17:27:43 -0400

    Created functions that create a new todo item and display all todo items

--- main.rb
+++ main.rb
@@ -6,13 +6,35 @@ require 'sinatra/reloader' if development?
 
 # List todo items
 get '/' do
+  @name = params[:name]
+  @description = params[:description]
+  @date = params[:date]
+  db = PG.connect(:dbname => 'Homework',
+                  :host => 'localhost',
+                  :user => 'postgres',
+                  :password => 'postgres')
+  sql = "select * from todo"
+  @todos = db.exec(sql)
+  db.close
   
   erb :todos
 end
 
 # Show the details of a todo
 get '/todo/:id' do
-  	erb :todo
+  @id = params[:id]
+  @name = params[:name]
+  @description = params[:description]
+  @date = params[:date]
+  db = PG.connect(:dbname => 'Homework',
+                  :host => 'localhost',
+                  :user => 'postgres',
+                  :password => 'postgres')
+  sql = "select * from todo where id = '#{@id}'"
+  @todo = db.exec(sql).first
+  db.close
+
+ 	erb :todo
 end
 
 # create todo
@@ -23,5 +45,24 @@ end
 # Create a todo by sending a POST request to this URL
 post '/create_todo' do
   #This will send you to the newly created todo
-  redirect to("/todo/#{id}")
+  @name = params[:name]
+  @description = params[:description]
+  @date = params[:date]
+  #@completed = params[:completed]
+  db = PG.connect(:dbname => 'Homework',
+                  :host => 'localhost',
+                  :user => 'postgres',
+                  :password => 'postgres')
+  sql = "insert into todo (name,description,date) values ('#{@name}','#{@description}','#{@date}')"# #{@completed}"
+  db.exec(sql)
+  db.close
+  redirect to("/")
+end
+
+get '/todo/:id/edit' do
+   erb :edit
+end
+
+post '/todo/:id/delete' do
+
 end
