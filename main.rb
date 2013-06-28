4982f509d750cc9967f9232898166e5648b012e9
Christine Coulter <christinetcoulter@gmail.com>
2013-06-28 17:19:02 -0400

    Create to_dos.sql file to create to_dos table in to_do_list database. Define function to connect to database in main.rb file.

--- main.rb
+++ main.rb
@@ -3,10 +3,17 @@ require 'sinatra'
 require 'pg'
 require 'sinatra/reloader' if development?
 
+# function to connect to the to_do_list database
+def get_todos(sql_input)
+  db = PG.connect(:dbname => 'to_do_list', :host => 'localhost')
+  result = db.exec(sql_input)
+  db.close
+  return result
+end
 
 # List todo items
 get '/' do
-  
+
   erb :todos
 end
 
