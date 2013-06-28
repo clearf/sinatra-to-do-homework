c232c09276c3db79f0d19e4bc9a51670c801e9f0
Theodore Cleveland <tcclevela@gmail.com>
2013-06-28 17:19:11 -0400

    accessed database info and stored in instance array @tasks

--- main.rb
+++ main.rb
@@ -4,34 +4,41 @@ require 'pg'
 require 'sinatra/reloader' if development?
 
 helper do
+	#opens a connection to the database
 	def open_connect
-		PG.connect(:dbname => 'address_book', :host => 'localhost')
+		PG.connect(:dbname => 'todo', :host => 'localhost')
 	end
 
+	#executes a sql string
 	def execute_sql (sql_string)
 		db = open_connect
 		db.exec(sql_string)
 		db.close
 	end
 end
+
 # List todo items
 get '/' do
-  
-  erb :todos
+	sql = "SELECT * FROM tasks"
+	@tasks = []
+	execute_sql(sql).each do |task|
+		@tasks << task
+	end
+	erb :todos
 end
 
 # Show the details of a todo
 get '/todo/:id' do
-  	erb :todo
+	erb :todo
 end
 
 # create todo
 get '/create_todo' do
-  erb :create_todo
+	erb :create_todo
 end
 
 # Create a todo by sending a POST request to this URL
 post '/create_todo' do
-  #This will send you to the newly created todo
-  redirect to("/todo/#{id}")
+	#This will send you to the newly created todo
+	redirect to("/todo/#{id}")
 end
