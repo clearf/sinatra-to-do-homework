b28ab78302d39d45632669bf6742d13e3f1a0334
Anuj Jhunjhunwala <anuj.jhunjhunwala@gmail.com>
2013-06-29 19:18:58 -0400

    Added more functionality to main.rb; added form for adding new tasks

--- main.rb
+++ main.rb
@@ -15,19 +15,26 @@ get '/' do
 end
 
 # Show the details of a todo
+# 1)
 get '/todo/:id' do
-
-  	erb :todo
+  id = params[:id]
+  db = PG.connect(:dbname => 'todolist', :host => 'localhost')
+  sql = "SELECT * FROM contacts WHERE id = #{id}"
+  @list = db.exec(sql).first
+  db.close
+  	erb :todo #:TODO NEEDS TO BE FINISHED
 end
 
 # create todo
+# i think can keep this like this
 get '/create_todo' do
-
   erb :create_todo
 end
 
 # Create a todo by sending a POST request to this URL
 post '/create_todo' do
-  #This will send you to the newly created todo
+  task = params[:task]
+  due_date = params[:due_date]
+  priority = params[:priority]
   redirect to("/todo/#{id}")
 end
