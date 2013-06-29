8b2f9e6adab2ae9499c74a8c31f0b172df4a5c25
Jamal Powell <jamalkpowell@gmail.com>
2013-06-29 19:03:33 -0400

    working on displaying description

--- main.rb
+++ main.rb
@@ -14,10 +14,6 @@ get '/' do
   erb :todos
 end
 
-# Show the details of a todo
-get '/todo/:id' do
-  	erb :todo
-end
 
 # create todo
 get '/create_todo' do
@@ -28,4 +24,14 @@ end
 post '/create_todo' do
   #This will send you to the newly created todo
   redirect to("/todo/#{id}")
+end
+
+# Show the details of a todo
+get '/todo/:id' do
+  @id = params[:id]
+  db = PG.connect(:dbname => 'errands', :host => 'localhost')
+  sql = "SELECT * FROM to_do WHERE id = #{id}"
+  @to_do = db.exec(sql).first
+  db.close
+  	erb :todo
 end
