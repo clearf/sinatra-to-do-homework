76904b2f1b7a406159f8f75800cfa1ab6a87d9bf
Anuj Jhunjhunwala <anuj.jhunjhunwala@gmail.com>
2013-06-29 16:46:49 -0400

    Added ability to list contacts on maing page

--- main.rb
+++ main.rb
@@ -5,18 +5,24 @@ require 'sinatra/reloader' if development?
 
 
 # List todo items
+# 1) should be good, need to create a database first
 get '/' do
-  
+  db = PG.connect(:dbname => 'todolist', :host => 'localhost')
+  sql = "SELECT * FROM list"
+  @list = db.exec(sql)
+  db.close
   erb :todos
 end
 
 # Show the details of a todo
 get '/todo/:id' do
+
   	erb :todo
 end
 
 # create todo
 get '/create_todo' do
+
   erb :create_todo
 end
 
