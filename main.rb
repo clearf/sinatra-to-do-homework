ce51b25d476478dec3bebed2dfda9d6c0e9d8b96
Christopher Wright <cmwright33@gmail.com>
2013-06-29 17:53:54 -0400

    fixed todo.erb method errors and routing problems

--- main.rb
+++ main.rb
@@ -17,8 +17,8 @@ end
 get '/todo/:id' do
   id = params[:id]
   db = PG.connect(:dbname => 'hw_sinatra', :host => 'localhost')
-  sql = "SELECT * FROM to_do_list WHERE id = #{id}"
-  @todos = db.exec(sql).first
+  sql = "select * from to_do_list where id = #{id}"
+  @todo = db.exec(sql).first
   db.close
   erb :todo
 end
@@ -30,6 +30,11 @@ end
 
 # Create a todo by sending a POST request to this URL
 post '/create_todo' do
+  id = params[:id]
+  db = PG.connect(:dbname => 'hw_sinatra' :host => 'localhost')
+  sql = "insert from to_do_list where id = #{id}"
+  db.exec(sql)
+  db.close
   #This will send you to the newly created todo
   redirect to("/todo/#{id}")
 end
