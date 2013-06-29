0eb61688b4189b4131f4c7cad3458617e807ec49
alicem <alice.mottola@gmail.com>
2013-06-29 16:24:48 -0400

    Done except that placing an apostrophe in any field makes an error

--- main.rb
+++ main.rb
@@ -20,7 +20,9 @@ end
 
 # Show the details of a todo
 get '/todo/:id' do
-  	erb :todo
+  id = params[:id]
+  @task = run_sql("SELECT * FROM todolist WHERE id = #{id}").first
+  erb :todo
 end
 
 # Create a todo by sending a POST request to this URL
@@ -30,3 +32,29 @@ post '/create_todo' do
   run_sql("INSERT INTO todolist (task, notes, done) VALUES ('#{@task}', '#{@notes}', false)")
   redirect to("/")
 end
+
+get '/edit/:id' do
+  @id = params[:id]
+  @task = run_sql("SELECT * FROM todolist WHERE id = #{@id}").first
+
+  erb :edit
+end
+
+post '/edit/:id' do
+  id = params[:id]
+  task = params[:task]
+  notes = params[:notes]
+  done = params[:done]
+  run_sql("UPDATE todolist SET (task, notes, done) = ('#{task}', '#{notes}', '#{done}') WHERE id = #{id}")
+
+  redirect to("/todo/#{id}")
+end
+
+post '/delete/:id' do
+  id = params[:id]
+  run_sql("DELETE FROM todolist WHERE id = #{id}")
+
+  redirect to('/')
+end
+
+
