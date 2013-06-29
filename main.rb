0510fb1dee8f7abe0d1e05cd071f1dc74e9d4b6d
Beryl <beryl.schragger@gmail.com>
2013-06-29 17:09:14 -0400

    Add comments to completed code.

--- main.rb
+++ main.rb
@@ -5,6 +5,7 @@ require 'sinatra/reloader' if development?
 require 'rainbow'
 
 # creating function to enter sql code more easily
+# had difficulty getting run_sql function to work
 helpers do
   def run_sql(sql)
     db = PG.connect(:dbname => 'todo', :host => 'localhost')
@@ -17,6 +18,7 @@ end
 
 # List todo items
 get '/' do
+  # setting up database connection for main page - displaying full list
   @db = PG.connect(:dbname => 'todo', :host => 'localhost')
   @show_db = 'select * from list'
   erb :todos
@@ -24,6 +26,7 @@ end
 
 # Show the details of a todo
 get '/todo/:id' do
+  # setting up database connection for task page - displaying item with certain id
   @db = PG.connect(:dbname => 'todo', :host => 'localhost')
   @item = "select * from list where id = #{params[:id]}"
   erb :todo
