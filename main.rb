f8dbfe607fb86504f2daadf115de8f4bfe7c21c5
Jamal Powell <jamalkpowell@gmail.com>
2013-06-29 20:01:10 -0400

    added edit and delete functions

--- main.rb
+++ main.rb
@@ -5,7 +5,7 @@ require 'sinatra/reloader' if development?
 
 
 # List todo items
-get '/' do
+get '/todos' do
 
   db = PG.connect(:dbname => 'errands', :host => 'localhost')
   sql = "SELECT * FROM to_do"
@@ -18,21 +18,44 @@ end
 # create todo
 get '/create_todo' do
 
-
   erb :create_todo
 end
 
 # Create a todo by sending a POST request to this URL
 post '/create_todo' do
   #This will send you to the newly created todo
-  redirect to("/todo/#{id}")
+  name = params[:name]
+  description = params[:description]
+  sql = "Insert into to_do(name, description) VALUES ('#{name}', '#{description}')"
+  db = PG.connect(:dbname => 'errands', :host => 'localhost')
+  db.exec(sql)
+  db.close
+  redirect to("/todos/#{id}")
+end
+
+post '/todos/:id/delete' do
+  id = params[:id]
+  db = PG.connect(:dbname => 'errands', :host => 'localhost')
+  sql = "DELETE FROM to_do WHERE id = #{id}"
+  @to_do = db.exec(sql).first
+  db.close
+  redirect to '/todos'
+end
+
+get '/todos/:id/edit' do
+  @id = params[:id]
+  db = PG.connect(:dbname => 'errands', :host => 'localhost')
+  sql = "SELECT * FROM to_do WHERE id = #{@id}"
+  @to_do = db.exec(sql).first
+  db.close
+  erb :edit
 end
 
 # Show the details of a todo
-get '/todo/:id' do
+get '/todos/:id' do
   @id = params[:id]
   db = PG.connect(:dbname => 'errands', :host => 'localhost')
-  sql = "SELECT * FROM to_do WHERE id = #{id}"
+  sql = "SELECT * FROM to_do WHERE id = #{@id}"
   @to_do = db.exec(sql).first
   db.close
   	erb :todo
