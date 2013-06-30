18550a4203b25f45d3bd6efe8ada53506b620d25
Christopher Wright <cmwright33@gmail.com>
2013-06-30 18:03:19 -0400

    corrected delete route, delete.erb

--- main.rb
+++ main.rb
@@ -13,10 +13,6 @@ get '/' do
   erb :todos
 end
 
-# create todo
-get '/create_todo' do
-  erb :create_todo
-end
 
 # Create a todo by sending a POST request to this URL
 post '/' do
@@ -32,6 +28,28 @@ post '/' do
   redirect to("/todo/#{id}")
 end
 
+post '/todo/:id/delete' do
+  id = params[:id]
+  db = PG.connect(:dbname => 'hw_sinatra', :host => 'localhost')
+  sql = "delete from to_do_list where id = #{id}"
+  db.exec(sql)
+  db.close
+  redirect to "/"
+end
+
+get '/todo/:id/edit' do
+  id = params[:id]
+  db = PG.connect(:dbname => 'hw_sinatra', :host => 'localhost')
+  sql = "select * from to_do_list where id = #{id}"
+  @todo = db.exec(sql).first
+  db.close
+  erb :edit
+end
+
+# create todo
+get '/create_todo' do
+  erb :create_todo
+end
 
 # Show the details of a todo
 get '/todo/:id' do
@@ -44,30 +62,14 @@ get '/todo/:id' do
   erb :todo
 end
 
-post '/todo/delete' do
-  id = params[:id]
-  db = PG.connect(:dbname => 'hw_sinatra', :host => 'localhost')
-  db.exec(sql)
-  db.close
-  redirect to '/'
-
-
-get '/todo/:id/edit' do
-  id = params[:id]
-  db = PG.connect(:dbname => 'hw_sinatra', :host => 'localhost')
-  sql = "select * from to_do_list where id = #{id}"
-  @todo = db.exec(sql).first
-  db.close
-  erb :edit
-end
 
 post '/todo/:id' do
   id = params[:id]
   task = params[:task]
-  task_description[:task_description]
+  task_description = params[:task_description]
   completed = params[:completed]
   db = PG.connect(:dbname => 'hw_sinatra', :host => 'localhost')
-  sql = "update to_do_list set (task, task_description, completed) values ('#{task}', '#{task_description}', #{completed}) where id = #{id}"
+  sql = "update to_do_list set (task, task_description, completed) = ('#{task}', '#{task_description}',#{completed}) WHERE id = #{id}"
   db.exec(sql)
   db.close
 end
