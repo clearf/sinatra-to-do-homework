4717f6e171fa9214da3cb2057107b91a6af9e66b
Christopher Wright <cmwright33@gmail.com>
2013-06-30 01:17:00 -0400

    sorted out the create_todo.erb finally

--- main.rb
+++ main.rb
@@ -19,29 +19,31 @@ get '/create_todo' do
 end
 
 # Create a todo by sending a POST request to this URL
-post '/create_todo' do
+post '/' do
   id = params[:id]
   task = params[:task]
   task_description = params[:task_description]
   completed = params[:completed]
   db = PG.connect(:dbname => 'hw_sinatra', :host => 'localhost')
   sql = "insert into to_do_list (task, task_description, completed) values ('#{task}', '#{task_description}', #{completed})"
-  db.exec(sql)
+  @todos = db.exec(sql)
   db.close
   #This will send you to the newly created todo
-  redirect to("/todo/#{id}")
+  redirect to("/todo/#{@todos['id']}")
 end
+
 
 # Show the details of a todo
 get '/todo/:id' do
   id = params[:id]
   db = PG.connect(:dbname => 'hw_sinatra', :host => 'localhost')
-  sql = "select * from to_do_list where id = #{id}"
+  sql = "select * from to_do_list where id =#{id}"
   @todo = db.exec(sql).first
   puts "closing the database"
   db.close
   erb :todo
 end
+
 
 get '/todo/:id/edit' do
   id = params[:id]
