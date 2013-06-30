89a4786ea346be8008bd71cbc670d1d08ebdabb6
Christopher Wright <cmwright33@gmail.com>
2013-06-30 00:27:16 -0400

    created an edit page and moved the routes around on main.rb

--- main.rb
+++ main.rb
@@ -13,16 +13,6 @@ get '/' do
   erb :todos
 end
 
-# Show the details of a todo
-get '/todo/:id' do
-  id = params[:id]
-  db = PG.connect(:dbname => 'hw_sinatra', :host => 'localhost')
-  sql = "select * from to_do_list where id = #{id}"
-  @todo = db.exec(sql).first
-  db.close
-  erb :todo
-end
-
 # create todo
 get '/create_todo' do
   erb :create_todo
@@ -31,10 +21,35 @@ end
 # Create a todo by sending a POST request to this URL
 post '/create_todo' do
   id = params[:id]
+  task = params[:task]
+  task_description = params[:task_description]
+  completed = params[:completed]
   db = PG.connect(:dbname => 'hw_sinatra', :host => 'localhost')
-  sql = "insert from to_do_list where id = #{id}"
+  sql = "insert into to_do_list (task, task_description, completed) values ('#{task}', '#{task_description}', #{completed})"
   db.exec(sql)
   db.close
   #This will send you to the newly created todo
   redirect to("/todo/#{id}")
 end
+
+# Show the details of a todo
+get '/todo/:id' do
+  id = params[:id]
+  db = PG.connect(:dbname => 'hw_sinatra', :host => 'localhost')
+  sql = "select * from to_do_list where id = #{id}"
+  @todo = db.exec(sql).first
+  puts "closing the database"
+  db.close
+  erb :todo
+end
+
+get '/todo/:id/edit' do
+  id = params[:id]
+  db = PG.connect(:dbname => 'hw_sinatra', :host => 'localhost')
+  sql = "select * from contacts where id = #{id}"
+  @todo = db.exec(sql).first
+  db.close
+  erb :edit
+end
+
+
