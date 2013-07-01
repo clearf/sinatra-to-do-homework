3f97e37c7429b8201e941bcecf744abdc8cb7d9e
Clearfield Christopher <chris.clearfield@gmail.com>
2013-07-01 10:42:35 -0400

    Adding basic create and edit flow

--- main.rb
+++ main.rb
@@ -15,6 +15,15 @@ get '/todo/:id' do
   	erb :todo
 end
 
+get '/todo/:id/edit' do
+	# do this:
+	# query the database for the 
+	# id params[:id] and then
+	# set that record equal to @todo
+
+  	erb :create_dtodo
+end
+
 # create todo
 get '/create_todo' do
   erb :create_todo
@@ -22,6 +31,29 @@ end
 
 # Create a todo by sending a POST request to this URL
 post '/create_todo' do
-  #This will send you to the newly created todo
-  redirect to("/todo/#{id}")
+  todo_name=params[:todo_name]
+  description=params[:description]
+  sql_query="insert into todo (todo_name, description) 
+  values ('#{todo_name}', '#{description}')"
+  # XXX todo: open database
+  # db = pg.connect...
+  db.exec(sql_query)
+  # XXX todo: close database
+  # This will send you to 
+  # the newly created todo
+  # Get maximum ID in the database
+  # Can you just query the DB for the todo item that 
+  # matches name, description?
+  # Due date. 
+  # **False assurance of uniquity. BAD!**
+  # Query to access the ID we just inserted. Good
+  # ** False assurance of staticness. BAD **
+  # 	Hack: this is a hack: Someone might have added
+  # records
+  # new_id = select max(id) from todos; 
+  # We don't know exactly if we can get the id returned from 
+  # our insert into. We will research. 
+  # Just redirct to the "index" page
+  # redirect to("/todo/#{id}")
+  redirect to("/")
 end
