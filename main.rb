46cbaaac620274872235767943a64cff5f27ee77
darchcruise <darchcruise@yahoo.com>
2013-07-01 06:47:46 -0400

    database

--- main.rb
+++ main.rb
@@ -2,26 +2,48 @@ require 'pry'
 require 'sinatra'
 require 'pg'
 require 'sinatra/reloader' if development?
+require 'rainbow'
 
 
 # List todo items
 get '/' do
-  
+  redirect to('/todos')
+end
+
+get '/todos' do
+  db = PG.connect(:dbname => 'to_do', :host => 'localhost') # open db
+  sql = "SELECT * FROM todos" # code to grab info from table called todos, located in a database called to_do.
+  @todos = db.exec(sql) # execute code (copy) (taking from table from database, and pasting in this variable. Keep in mind the table is in an array that holds multiple hashes. Each hash is a row.)
+  db.close # close db
   erb :todos
 end
 
-# Show the details of a todo
-get '/todo/:id' do
-  	erb :todo
+post '/' do
+  task = params[:task]
+  due = params[:due]
+  priority = params[:priority]
+  completed = params[:completed]
+  sql = "INSERT INTO todos (task, due, priority, completed) VALUES ('#{task}', '#{due}', #{priority}, #{completed})" # storing code inside of variable.
+  db = PG.connect(:dbname => 'to_do', :host => 'localhost') # open db
+  db.exec(sql) # execute code (paste) (giving to database, which is why there is no variable needed)
+  db.close
+  redirect to '/'
 end
 
-# create todo
-get '/create_todo' do
-  erb :create_todo
+
+
+
+get '/:id/add' do
+  id = params[:id]
+  sql = "SELECT * FROM todos WHERE id= #{id}"
+  db = PG.connect(:dbname => 'to_do', :host => 'localhost')
+  @todo = db.exec(sql)[0]
+  db.close
+  erb :add
 end
 
-# Create a todo by sending a POST request to this URL
-post '/create_todo' do
-  #This will send you to the newly created todo
-  redirect to("/todo/#{id}")
-end
+
+
+
+
+
